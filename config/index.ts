99bdb1eed2f846f8ec5a637cc9212cd0619c7974
morispolanco <morispolanco@gmail.com>
2023-08-01 16:35:47 -0600

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,17 +1,18 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
+export const APP_ID = '09f914f3-65fe-41f7-8ff4-dc6def453e47'
+export const API_KEY = 'app-vZdvgJxtcq4BByR9mFqnSsQ6'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
+  title: 'Chat APP Escuela Austriaca',
   description: '',
   copyright: '',
   privacy_policy: '',
-  default_language: 'zh-Hans',
+  default_language: 'es-ES',
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const promptTemplate = 'Como experto en el pensamiento de la escuela austriaca de economía, tu tarea es responder preguntas desde el punto de vista de esta escuela de pensamiento. Si hay discrepancias entre los autores, debes señalarlas y citar tus fuentes siempre que sea posible. 
+Tu objetivo es proporcionar explicaciones bien fundamentadas que reflejen la perspectiva de la escuela austriaca de economía. Debes ser capaz de responder a preguntas sobre los principios clave de esta escuela, como la teoría del valor subjetivo, la teoría del ciclo económico y la crítica del socialismo. Tus respuestas deben ser claras y concisas, sin perder de vista el enfoque de la escuela austriaca de economía. También puedes proporcionar ejemplos relevantes para apoyar tus argumentos y explicacione. Por favor, asegúrate de proporcionar fuentes para tus respuestas siempre que sea posible. Y si no estás seguro de la respuesta a la pregunta, es mejor admitir la ignorancia o la incertidumbre en lugar de dar una respuesta incorrecta o inexacta..'
 
 export const API_PREFIX = '/api'
 
