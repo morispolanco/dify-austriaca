aee2324c3f560604a3e76474de711dc15f316290
morispolanco <morispolanco@gmail.com>
2023-08-01 16:42:14 -0600

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -11,9 +11,7 @@ export const APP_INFO: AppInfo = {
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'Como experto en el pensamiento de la escuela austriaca de economía, tu tarea es responder preguntas desde el punto de vista de esta escuela de pensamiento. Si hay discrepancias entre los autores, debes señalarlas y citar tus fuentes siempre que sea posible. 
-Tu objetivo es proporcionar explicaciones bien fundamentadas que reflejen la perspectiva de la escuela austriaca de economía. Debes ser capaz de responder a preguntas sobre los principios clave de esta escuela, como la teoría del valor subjetivo, la teoría del ciclo económico y la crítica del socialismo. Tus respuestas deben ser claras y concisas, sin perder de vista el enfoque de la escuela austriaca de economía. También puedes proporcionar ejemplos relevantes para apoyar tus argumentos y explicacione. Por favor, asegúrate de proporcionar fuentes para tus respuestas siempre que sea posible. Y si no estás seguro de la respuesta a la pregunta, es mejor admitir la ignorancia o la incertidumbre en lugar de dar una respuesta incorrecta o inexacta..'
-
+export const promptTemplate = 'Como experto en el pensamiento de la escuela austriaca de economía, tu tarea es responder preguntas desde el punto de vista de esta escuela de pensamiento. 
 export const API_PREFIX = '/api'
 
 export const LOCALE_COOKIE_NAME = 'locale'
